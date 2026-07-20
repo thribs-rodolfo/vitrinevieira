d3e29a1c96a3a47453b290f489e327a1959d5ee7
Thiago Silva <thiago@mykey.com.br>
2026-07-20 12:23:57 -0300

    altera mensagem do WhatsApp na página controledochaveiro

--- controledochaveiro/index.js
+++ controledochaveiro/index.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     btnWhatsapp.addEventListener("click", () => {
       const phoneNumber = "5512992223481"
       const textMessage = encodeURIComponent(
-        "Olá, Thiago! Acessei a página do Programa de Controle do Chaveiro da MyKey e gostaria de saber mais sobre o sistema com taxa única de instalação de R$ 100.",
+        "Olá, Thiago! Quero instalar o programa Controle do Chaveiro da MyKey e estou ciente da taxa única de instalação de R$ 100,00",
       )
 
       const targetUrl = `https://wa.me/${phoneNumber}?text=${textMessage}`
